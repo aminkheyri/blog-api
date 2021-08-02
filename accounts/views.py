0a22022d02f1b809d6fef8e6ca817b524a58188f
amin <kheyrriamin@yahoo.com>
2021-08-03 00:42:30 +0430

    User list

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth import get_user_model
+from .serializer import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
--- djangoProject/api_urls.py
+++ djangoProject/api_urls.py
@@ -1,6 +1,8 @@
 from rest_framework import routers
 from posts.views import PostViewSet
+from accounts.views import UserViewSet
 
 
 router = routers.SimpleRouter()
 router.register('posts', PostViewSet, basename='posts')
+router.register('accounts', UserViewSet, basename='accounts')
