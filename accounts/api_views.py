b2ef7e5212ad87ab5845d3d411c7f425f21a8bd2
amin <kheyrriamin@yahoo.com>
2021-08-11 05:42:43 +0430

    user serializer

--- accounts/api_views.py
+++ accounts/api_views.py
@@ -1,10 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth import get_user_model
 from .serializer import UserSerializer
-from djangoProject.permissions import IsSuperUserOrStaffReadOnly
+from djangoProject.permissions import IsSuperUser
 
 
 class UserViewSet(ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsSuperUserOrStaffReadOnly]
+    permission_classes = [IsSuperUser]
--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import permissions
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = "__all__"
+        fields = ('id', 'phone', 'email', 'first_name', 'last_name', 'last_login', 'is_superuser')
